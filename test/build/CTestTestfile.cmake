214d457ef0849df01d5f2be20765eed21e501add
Dongmin <emoonjy@naver.com>
2022-08-22 16:10:10 +0900

    test version

---
+++ test/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/dong/test/src
+# Build directory: /home/dong/test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("lidar_camera_calibration")
---
+++ test/build/lidar_camera_calibration/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dong/test/src/lidar_camera_calibration
+# Build directory: /home/dong/test/build/lidar_camera_calibration
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
